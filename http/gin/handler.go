197d7b2a795c4a6f0960fc4e23b034e6fc21f521
herui <hr3685930@126.com>
2022-07-28 13:26:48 +0800

    add http/rpc timeout

--- http/gin/handler.go
+++ http/gin/handler.go
@@ -2,12 +2,14 @@ package gin
 
 import (
 	"bytes"
+	"context"
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
 	"github.com/pkg/errors"
 	"io/ioutil"
 	"net/http"
+	"time"
 )
 
 var (
@@ -77,5 +79,27 @@ func ErrHandler(errorReport HTTPErrorReport) gin.HandlerFunc {
 			}
 		}
 
+	}
+}
+
+// TimeoutMiddleware timeout middleware wraps the request context with a timeout
+func TimeoutMiddleware(timeout time.Duration) func(c *gin.Context) {
+	return func(c *gin.Context) {
+		// wrap the request context with a timeout
+		ctx, cancel := context.WithTimeout(c.Request.Context(), timeout)
+
+		defer func() {
+			// check if context timeout was reached
+			if ctx.Err() == context.DeadlineExceeded {
+				// write response and abort the request
+				c.Writer.WriteHeader(http.StatusGatewayTimeout)
+				c.Abort()
+			}
+			//cancel to clear resources after finished
+			cancel()
+		}()
+		// replace request with context wrapped request
+		c.Request = c.Request.WithContext(ctx)
+		c.Next()
 	}
 }
--- rpc/grpc.go
+++ rpc/grpc.go
@@ -10,6 +10,9 @@ import (
 	"google.golang.org/grpc/status"
 	"net"
 	"runtime/debug"
+	"strings"
+	"sync"
+	"time"
 )
 
 type Grpc struct {
@@ -82,5 +85,52 @@ func Err(code codes.Code, msg string) *GrpcError {
 	return &GrpcError{
 		Err:   status.New(code, msg).Err(),
 		Stack: []byte(fmt.Sprintf("%+v\n", errors.New(msg))),
+	}
+}
+
+// UnaryTimeoutInterceptor returns a func that sets timeout to incoming unary requests.
+func UnaryTimeoutInterceptor(timeout time.Duration) grpc.UnaryServerInterceptor {
+	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo,
+		handler grpc.UnaryHandler) (interface{}, error) {
+		ctx, cancel := context.WithTimeout(ctx, timeout)
+		defer cancel()
+
+		var resp interface{}
+		var err error
+		var lock sync.Mutex
+		done := make(chan struct{})
+		// create channel with buffer size 1 to avoid goroutine leak
+		panicChan := make(chan interface{}, 1)
+		go func() {
+			defer func() {
+				if p := recover(); p != nil {
+					// attach call stack to avoid missing in different goroutine
+					panicChan <- fmt.Sprintf("%+v\n\n%s", p, strings.TrimSpace(string(debug.Stack())))
+				}
+			}()
+
+			lock.Lock()
+			defer lock.Unlock()
+			resp, err = handler(ctx, req)
+			close(done)
+		}()
+
+		select {
+		case p := <-panicChan:
+			panic(p)
+		case <-done:
+			lock.Lock()
+			defer lock.Unlock()
+			return resp, err
+		case <-ctx.Done():
+			err := ctx.Err()
+
+			if err == context.Canceled {
+				err = status.Error(codes.Canceled, err.Error())
+			} else if err == context.DeadlineExceeded {
+				err = status.Error(codes.DeadlineExceeded, err.Error())
+			}
+			return nil, err
+		}
 	}
 }
